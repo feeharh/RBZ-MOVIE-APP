4f4fbfcff74301ca6058905b5c16bbf4a88bf3b7
zebiba <zzegherghish>
2021-11-13 14:22:59 -0600

    Movie App Startup proj

---
+++ rbzmovie-frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: 
+  `
+  <h1>Movie App Skeleton</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'rbzmovie-frontend';
+}
