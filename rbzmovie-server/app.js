d37ad1e9c255118d031a4434735f402b06e686f6
Rofiat Adesewa Adepoju <finzysewa@gmail.com>
2021-11-13 17:32:19 -0600

    server configurations

--- rbzmovie-server/app.js
+++ rbzmovie-server/app.js
@@ -1,9 +1,17 @@
-const helmet = require('helmet');
+//imports
 const express = require('express');
-const app = express();
+const helmet = require('helmet');
 require('./data/db')
-const port = 3000;
+const port = 4000;
 const routes = require('./route/route');
+
+//instantiation
+const app = express();
+
+//configurations
+app.set('strict routing', true);
+app.set('case sensitive routing',true);
+app.set('x-powered-by',false);
 
 //Middleware
 app.use(express.json());
@@ -13,7 +21,14 @@ app.use(helmet());
 app.use('/api' , routes);
 app.all("*", (req, res, next) => {
     next(new Error(`No route found`));
-  });
+});
+
+//error handler
+app.use(function(err,req,res,next){
+  res.json({
+    msg:err.message
+  })
+})
 
 app.listen(port, ()=>{
     console.log(`Listening on: ${port}`);
