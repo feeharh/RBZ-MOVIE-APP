12c0484ec7004a16ae3ca30c6bf0b745c0fcc5ca
zebiba <zzegherghish>
2021-11-15 19:54:11 -0600

    login and signup pushed

---
+++ rbzmovie-frontend/src/app/services/login/login.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private url = 'http://localhost:4600/api/users';
+
+
+  constructor(private httpclient: HttpClient) { }
+
+  loginUser(body): Observable<any> {
+    // const model = { email: body.email, password: body.password };
+    // console.log(model);
+    return this.httpclient
+      .post(this.url + '/login', body, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Access-Control-Allow-Headers': 'Content-Type'
+        })
+      });
+
+  }
+
+  logoutUser(userId) {
+    console.log('logout handler service')
+    return this.httpclient
+      .get(this.url + `/logout`)
+
+
+  }
+}
